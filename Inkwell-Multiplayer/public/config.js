1a73124a3d4ca3db080fdaec14b8403ef6b0a253
Mohammed Sulthan G <msulthan139@gmail.com>
2026-08-12 19:19:17 +0530

    Update config.js

--- Inkwell-Multiplayer/public/config.js
+++ Inkwell-Multiplayer/public/config.js
@@ -1,3 +1,3 @@
 // Set this to your Render Web Service URL after deploying the backend.
 // Example: https://inkwell-server.onrender.com
-window.INKWELL_SERVER_URL = 'http://localhost:10000';
+window.INKWELL_SERVER_URL = 'https://inkwell-server-q1g7.onrender.com';
